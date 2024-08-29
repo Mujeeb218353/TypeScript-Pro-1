b8c527bb0e1a1e39356dfcd7cbb393c773c02e8d
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-08-30 03:28:18 +0500

    added search functionality

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
-const getUser = document.querySelector('#user');
+const getUser = document.querySelector("#user");
 const formSubmit = document.querySelector("#form");
-const mainContainer = document.querySelector('.main-container');
+const mainContainer = document.querySelector(".main-container");
 const reusableFunction = async (url, options) => {
     const response = await fetch(url, options);
     if (!response.ok) {
@@ -12,11 +12,11 @@ const reusableFunction = async (url, options) => {
     return data;
 };
 const showCard = (user) => {
-    const main_container = document.getElementById('main-container');
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const main_container = document.getElementById("main-container");
+    const card = document.createElement("div");
+    card.classList.add("card");
     card.innerHTML = `
-            <img src="${user.avatar_url}" alt="${'avatar_img'}">
+            <img src="${user.avatar_url}" alt="${"avatar_img"}">
             <p>${user.login}</p>
             <a href="${user.html_url}" target="_blank">Visit Profile</a>
     `;
@@ -29,4 +29,29 @@ const fetchUserData = (url) => {
         }
     });
 };
-const data = fetchUserData('https://api.github.com/users');
+fetchUserData("https://api.github.com/users");
+formSubmit?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = getUser.value.toLowerCase();
+    try {
+        const url = `https://api.github.com/users`;
+        const allUsers = await reusableFunction(url, {});
+        const filteredUsers = allUsers.filter((user) => {
+            return user.login.toLowerCase().includes(username);
+        });
+        const mainContainer = document.getElementById("main-container");
+        mainContainer.innerHTML = "";
+        if (filteredUsers.length === 0) {
+            mainContainer.innerHTML = `<h3>No user found</h3>`;
+        }
+        else {
+            for (const user of filteredUsers) {
+                showCard(user);
+            }
+        }
+    }
+    catch (error) {
+        alert("Something went wrong");
+        window.location.reload();
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,73 @@
-const getUser = document.querySelector('#user') as HTMLInputElement;
-const formSubmit: HTMLFormElement | null = document.querySelector("#form")
-const mainContainer = document.querySelector('.main-container') as HTMLElement;
+const getUser = document.querySelector("#user") as HTMLInputElement;
+const formSubmit: HTMLFormElement | null = document.querySelector("#form");
+const mainContainer = document.querySelector(".main-container") as HTMLElement;
 
 interface UserData {
-    id: number
-    avatar_url: string
-    login: string
-    html_url: string
+  id: number;
+  avatar_url: string;
+  login: string;
+  html_url: string;
 }
 
-const reusableFunction = async <T>(url: string, options?: RequestInit): Promise<T> => {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-        throw new Error(response.statusText)
-    }
-    const data = response.json()
-    console.log(data);
-    return data
-}
+const reusableFunction = async <T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> => {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const data = response.json();
+  console.log(data);
+  return data;
+};
 
 const showCard = (user: UserData) => {
-    const main_container = document.getElementById('main-container') as HTMLDivElement
-    const card = document.createElement('div') as HTMLDivElement
-    card.classList.add('card')
-    card.innerHTML = `
-            <img src="${user.avatar_url}" alt="${'avatar_img'}">
+  const main_container = document.getElementById(
+    "main-container"
+  ) as HTMLDivElement;
+  const card = document.createElement("div") as HTMLDivElement;
+  card.classList.add("card");
+  card.innerHTML = `
+            <img src="${user.avatar_url}" alt="${"avatar_img"}">
             <p>${user.login}</p>
             <a href="${user.html_url}" target="_blank">Visit Profile</a>
-    `
-    main_container.appendChild(card);
-}
+    `;
+  main_container.appendChild(card);
+};
 
 const fetchUserData = (url: string) => {
-    reusableFunction<UserData[]>(url, {}).then((users) => {
-        for(const user of users) {
-            showCard(user)
-            
-        }
-    })
-}
+  reusableFunction<UserData[]>(url, {}).then((users) => {
+    for (const user of users) {
+      showCard(user);
+    }
+  });
+};
 
-const data = fetchUserData('https://api.github.com/users');
+fetchUserData("https://api.github.com/users");
+
+formSubmit?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = getUser.value.toLowerCase();
+  try {
+    const url = `https://api.github.com/users`;
+    const allUsers = await reusableFunction<UserData[]>(url, {});
+    const filteredUsers = allUsers.filter((user) => {
+        return user.login.toLowerCase().includes(username);
+    })
+    const mainContainer = document.getElementById(
+        "main-container"
+    ) as HTMLDivElement;
+    mainContainer.innerHTML = "";
+    if (filteredUsers.length === 0) {
+        mainContainer.innerHTML = `<h3>No user found</h3>`;
+    } else {
+        for (const user of filteredUsers) {
+            showCard(user);
+        }
+    }
+  } catch (error) {
+    alert("Something went wrong");
+    window.location.reload();
+  }
+});
